4b9a92cc93a04931fd522c84b5f58275a00ab56f
Aditya Thakkar <adityathakkar29@gmail.com>
2022-05-03 10:19:07 -0500

    CORS changes-4

--- la_api/main.py
+++ la_api/main.py
@@ -4,7 +4,10 @@ import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+# from fastapi.middleware.cors import CORSMiddleware
+
+from starlette.middleware import Middleware
+from starlette.middleware.cors import CORSMiddleware
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -15,25 +18,32 @@ from routers import subscribers
 from routers import events
 from routers import rsvp
 
-app = FastAPI()
-
 origins = [
-    # "http://lohanadfw.org",
-    # "https://lohanadfw.org",
-    # "http://www.lohanadfw.org",
-    # "https://www.lohanadfw.org",
     "https://lohanadfw.org",
     "http://lohanadfw.org",
     "https://www.lohanadfw.org",
     "http://www.lohanadfw.org"
 ]
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    # allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+
+middlewares = [
+    Middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=['*'],
+        allow_headers=['*']
+    )
+]
+
+app = FastAPI(middlewares=middlewares)
+
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     # allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
 db_client = DBClient()
 app.include_router(committee.router)
